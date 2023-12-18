9b2c4b0382954981dec00e4779dc9c85cefadf75
Simon Adjei <83459909+simonn70@users.noreply.github.com>
2023-12-18 03:25:17 -0800

    Delete Desktop/commerce_gig/E-commerce_Frontend directory

--- Desktop/commerce_gig/E-commerce_Frontend/src/redux/store.tsx
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import { authApiSlice } from "./slice/authApiSlice";
-
-export const store = configureStore({
-  reducer: {
-    auth: authApiSlice,
-  },
-});
